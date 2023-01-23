f86983d9656f1eb485b4536c6e3f9ad17ae2f60f
gulchizxvr <kyrylo.hulchenko@gmail.com>
2023-01-23 19:21:52 +0200

    add netlify setting

--- src/service/axios.service.ts
+++ src/service/axios.service.ts
@@ -10,12 +10,6 @@ axiosService.interceptors.request.use((config) => {
     const access = "54c8e5fa4d104f2f9ea178a688c8f6ea";
     const {headers} = config
     headers!.Authorization = `Bearer ${access}`
-    // @ts-ignore
-    config!.headers['Access-Control-Allow-Origin'] = '*';
-    // @ts-ignore
-    config.headers['Access-Control-Allow-Credentials'] = 'true'
-    // @ts-ignore
-    config.headers['Access-Control-Request-Method'] = 'POST'
     return config
 })
 
