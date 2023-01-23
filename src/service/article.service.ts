41e691b2abdffdf31dee39ae6626854f3719bcf8
gulchizxvr <kyrylo.hulchenko@gmail.com>
2023-01-23 19:34:03 +0200

    add netlify setting

--- src/service/article.service.ts
+++ src/service/article.service.ts
@@ -5,7 +5,7 @@ import {baseURL, urls} from "../configs";
 
 
 const articleService = {
-    getAllArticle: ():AxiosRes<IArticles> => axiosService.get(baseURL+urls.all,{params:{language:"en",pageSize:21,apiKey:"54c8e5fa4d104f2f9ea178a688c8f6ea"}}),
+    getAllArticle: ():AxiosRes<IArticles> => axiosService.get(baseURL+urls.all,{params:{pageSize:21,apiKey:"54c8e5fa4d104f2f9ea178a688c8f6ea"}}),
     searchArticles: (value:string):AxiosRes<IArticles> => axiosService.get(baseURL+"/everything", {params:{q:value,searchIn:"title,description"}})
 
 }
